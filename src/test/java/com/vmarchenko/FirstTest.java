032f72f65ad0d1a23a45491b4693796ebb32a79d
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-12-03 17:49:24 +0200

    Add first dummy tests with Appium

---
+++ src/test/java/com/vmarchenko/FirstTest.java
@@ -0,0 +1,41 @@
+package com.vmarchenko;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.0.0");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", new File("apks/org.wikipedia.apk").getAbsolutePath());
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @Test
+    public void firstTest() {
+        System.out.println("First Test Run");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
